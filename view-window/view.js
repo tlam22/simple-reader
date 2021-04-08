a9fe2c6fd0c20c7232fe9c97b4d4ceff0ce61d0d
tsz.c.lam <tsz.c.lam@avanade.com>
2021-04-08 15:18:29 -0500

    fix reload view window

--- view-window/view.js
+++ view-window/view.js
@@ -1,41 +1,56 @@
 window.jQuery = window.$ = jQuery;
 
 $(document).ready(function () {
+  use_cached_data();
   window.addEventListener('message', evt => {
     if (evt.data.type === 'view-window-data') {
       load_images(evt.data.value)
     }
   })
-
-  function load_images(data) {
-    let output = "";
-    let title = data.title;
-    let images = data.images
-    for (const image of images) {
-      output += `<div>`
-      output += `<img src='file://${image}'>`
-      output += `</div>`
-    }
-    $('#g').html(output);
-    document.title = title;
-    if(data.styles){
-      $('#g').attr('style',data.styles.style)
-    }
-    check_size();
-  }
-
-  function check_size() {
-    let foo = document.getElementById('g');
-    let g = $('#g');
-    let observer = new MutationObserver(function (mutations) {
-      window.postMessage({
-        type: 'view-g-div-resize',
-        value: {style: g.attr('style')}
-      })
-    });
-    observer.observe(foo, {
-      attributes: true,
-      attributeFilter: ['style']
-    });
-  }
 });
+
+function load_images(data) {
+  let output = "";
+  let title = data.title;
+  let images = data.images
+  for (const image of images) {
+    output += `<div>`
+    output += `<img src='file://${image}'>`
+    output += `</div>`
+  }
+  $('#g').html(output);
+  document.title = title;
+  if(data.styles){
+    $('#g').attr('style',data.styles.style)
+  }
+  check_size();
+  cache_data(data);
+}
+
+function cache_data(data){ 
+  $(window).bind('beforeunload', function(){
+    window.sessionStorage.setItem('image-data', JSON.stringify(data));
+  })
+}
+
+function use_cached_data(){
+  let data = window.sessionStorage.getItem('image-data');
+  if(data){
+    load_images(JSON.parse(data));
+  }
+}
+
+function check_size() {
+  let foo = document.getElementById('g');
+  let g = $('#g');
+  let observer = new MutationObserver(function (mutations) {
+    window.postMessage({
+      type: 'view-g-div-resize',
+      value: {style: g.attr('style')}
+    })
+  });
+  observer.observe(foo, {
+    attributes: true,
+    attributeFilter: ['style']
+  });
+}
